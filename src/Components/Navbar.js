8e966d1ed59e19710c48b98c7a3279bf682cfd3a
saga-r-code <shah.sagar2006@gmail.com>
2024-07-28 17:40:46 +0530

    add style

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,15 +3,14 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="p-3">
-      <div className="nav flex justify-around py-2">
+    <div className="p-3 bg-gray-900 text-white">
+      <div className="nav flex justify-around py-2 ">
         <h1 className="text-2xl font-semibold"><Link to="/">INotebook</Link></h1>
         <div className="menu list-none flex gap-10 text-[17px]">
           <li><Link to="/" >Home</Link></li>
           <li><Link to="/about" >About</Link></li>
         </div>
       </div>
-      <div className="w-full h-[2px] bg-slate-200"/>
     </div>
   );
 };
